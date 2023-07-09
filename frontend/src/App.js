c45a30a9de300ab1ad091a990383c415182d5ea4
HaroldCV <70811385+HaroldCV@users.noreply.github.com>
2023-07-09 13:49:35 -0500

    cambios_estilos_ App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import Swal from 'sweetalert2';
+import './App.css';
 
 function App() {
   const [file, setFile] = useState(null);
@@ -6,9 +9,15 @@ function App() {
   const [method, setMethod] = useState('');
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+
+    // Mostrar la imagen seleccionada
+    const imageUrl = URL.createObjectURL(selectedFile);
+    setSelectedImage(imageUrl);
   };
 
   const handleKChange = (event) => {
@@ -20,6 +29,15 @@ function App() {
   };
 
   const handleSubmit = () => {
+    if (!file) {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Please select a file.',
+        icon: 'error',
+      });
+      return;
+    }
+
     setLoading(true);
     const formData = new FormData();
     formData.append('archivo', file);
@@ -34,6 +52,14 @@ function App() {
       .then((data) => {
         setResult(data);
         setLoading(false);
+
+        if (data.ok) {
+          Swal.fire({
+            title: 'Success!',
+            text: 'Processing completed successfully.',
+            icon: 'success',
+          });
+        }
       })
       .catch((error) => {
         console.error(error);
@@ -42,30 +68,46 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>File Upload</h1>
-      <div>
-        <label htmlFor="file">Choose file:</label>
-        <input type="file" id="file" onChange={handleFileChange} />
+    <div className="container">
+      <h1 className="styled-title">Face Recognition</h1>
+      <div className="form-group">
+        <label htmlFor="file" className="file-label">
+          <span className="file-label-text">Choose file:</span>
+          <input type="file" id="file" className="file-input" onChange={handleFileChange} />
+          <span className="file-input-button">Browse</span>
+        </label>
       </div>
-      <div>
+      {selectedImage && (
+        <div className="selected-image">
+          <h2>Selected Image</h2>
+          <img className="selected-image-item" src={selectedImage} alt="Selected" />
+        </div>
+      )}
+      <div className="form-group">
         <label htmlFor="k">K:</label>
-        <input type="number" id="k" value={k} onChange={handleKChange} />
+        <input type="number" id="k" value={k} onChange={handleKChange} className="styled-input" />
       </div>
-      <div>
+
+      <div className="form-group">
         <label htmlFor="method">Method:</label>
-        <select id="method" value={method} onChange={handleMethodChange}>
+        <select
+          id="method"
+          value={method}
+          onChange={handleMethodChange}
+          className="styled-select"
+        >
           <option value="knnsearch">knnsearch</option>
           <option value="knnrtree">knnrtree</option>
           <option value="knnkdtree">knnkdtree</option>
         </select>
       </div>
-      <button onClick={handleSubmit}>Submit</button>
+      <button className="styled-button" onClick={handleSubmit}>
+        Submit
+      </button>
       {loading && <p>Loading...</p>}
       {result && (
         <div>
-          <h2>Result</h2>
-          <p>{result.ok ? 'Success' : 'Error'}: {result.msg}</p>
+          <h2 className="styled-title">Result</h2>
           {result.data && result.data.length > 0 ? (
             <>
               <p>Data:</p>
@@ -78,8 +120,8 @@ function App() {
                         src={process.env.PUBLIC_URL + '/dataset/' + imagePath}
                         alt={`Image ${index + 1}`}
                       />
+                      <p className="image-path">{imagePath}</p>
                       <p>Score: {score}</p>
-                      <p>Image Path: {imagePath}</p>
                     </div>
                   );
                 })}
